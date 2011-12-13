7c6f047657d33e7c91180e895f592715bf61c619
Michael Cameron <michael.e.cameron@gmail.com>
2011-12-13 11:03:57 -0600

    Bump version number

--- JesqueGrailsPlugin.groovy
+++ JesqueGrailsPlugin.groovy
@@ -15,7 +15,7 @@ import grails.util.GrailsUtil
 
 class JesqueGrailsPlugin {
 
-    def version = "0.3.0.M6"
+    def version = "0.3.0.M7"
     def grailsVersion = "1.3.0 > *"
     def dependsOn = [redis: "1.0.0M7 > *", hibernate: "1.3.6 > *"]
     def pluginExcludes = [
