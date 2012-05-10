262fe5ec79173d1488b7bb375c0da981d7f513e9
Michael Cameron <michael.e.cameron@gmail.com>
2012-05-10 11:01:18 -0500

    Update release plugin, release 0.4.0-SNAPSHOT

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -39,6 +39,9 @@ grails.project.dependency.resolution = {
     }
     plugins {
         compile(':redis:1.0.0.M9')
+        compile(':release:2.0.0', ':rest-client-builder:1.0.2') {
+            export = false
+        }
         test(":spock:0.6-SNAPSHOT") {
             export = false
         }
