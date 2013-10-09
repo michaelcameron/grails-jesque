c993f0a7e271062e93963f19112330cc9a8938d4
Philipp <philipp@uberall.com>
2013-10-09 16:14:55 +0200

    added functionality to specify a custom worker and listener

--- grails-app/services/grails/plugin/jesque/JesqueService.groovy
+++ grails-app/services/grails/plugin/jesque/JesqueService.groovy
@@ -126,8 +126,25 @@ class JesqueService implements DisposableBean {
                        boolean paused = false)
     {
         log.debug "Starting worker processing queueus: ${queues}"
-        def worker = new GrailsWorkerImpl(grailsApplication, jesqueConfig, queues, jobTypes)
-        if( exceptionHandler )
+
+        Class customWorkerClass = grailsApplication.config.grails.jesque.custom.worker.clazz
+        Worker worker
+        if (customWorkerClass && customWorkerClass in GrailsWorkerImpl) {
+            worker = customWorkerClass.newInstance(grailsApplication, jesqueConfig, queues, jobTypes)
+        } else {
+            if (customWorkerClass)
+                log.warn('The specified custom worker class does not extend GrailsWorkerImpl. Ignoring it')
+            worker = new GrailsWorkerImpl(grailsApplication, jesqueConfig, queues, jobTypes)
+        }
+
+        Class customListenerClass = grailsApplication.config.grails.jesque.custom.listener.clazz
+        if (customListenerClass && customListenerClass in WorkerListener) {
+            worker.addListener(customListenerClass.newInstance() as WorkerListener)
+        } else if (customListenerClass) {
+            log.warn('The specified custom listener class does not implement WorkerListener. Ignoring it')
+        }
+
+        if (exceptionHandler)
             worker.exceptionHandler = exceptionHandler
 
         if (paused) {
@@ -181,28 +198,23 @@ class JesqueService implements DisposableBean {
 
     void startWorkersFromConfig(ConfigObject jesqueConfigMap) {
 
-        Boolean boolStartPaused = jesqueConfigMap.startPaused as boolean ?: false
+        def startPaused = jesqueConfigMap.startPaused as boolean ?: false
 
-        jesqueConfigMap.workers.each{ String workerPoolName, value ->
+        jesqueConfigMap.workers.each { String workerPoolName, value ->
             log.info "Starting workers for pool $workerPoolName"
 
             def workers = value.workers ? value.workers.toInteger() : DEFAULT_WORKER_POOL_SIZE
+            def queueNames = value.queueNames
+            def jobTypes = value.jobTypes
 
-            if( !((value.queueNames instanceof String) || (value.queueNames instanceof List<String>)))
+            if (!((queueNames instanceof String) || (queueNames instanceof List<String>)))
                 throw new Exception("Invalid queueNames for pool $workerPoolName, expecting must be a String or a List<String>.")
 
-            if( !(value.jobTypes instanceof Map) )
-                throw new Exception("Invalid jobTypes (${value.jobTypes}) for pool $workerPoolName, must be a map")
-
-            WorkerListener customListener = null
-            if (value.customListener) {
-                customListener = ((Class) value.customListener).newInstance()
-            }
+            if (!(jobTypes instanceof Map))
+                throw new Exception("Invalid jobTypes (${jobTypes}) for pool $workerPoolName, must be a map")
 
             workers.times {
-                def worker = startWorker(value.queueNames, value.jobTypes, (ExceptionHandler) null, boolStartPaused.booleanValue())
-                if (worker && customListener)
-                    worker.addListener(customListener)
+                startWorker(queueNames, jobTypes, null, startPaused)
             }
         }
     }
